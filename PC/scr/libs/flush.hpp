e7df3a9824cb8562b7857bf5f66be137e6c36b64
Danylo <danaroz1234@gmail.com>
2026-01-18 16:43:20 +0200

    protocol implement

--- PC/scr/libs/flush.hpp
+++ PC/scr/libs/flush.hpp
@@ -9,10 +9,6 @@
 #include <sys/ioctl.h>
 
 
-
-
-
-
 int write_to_display(std::vector<uint8_t> &buf)
 {
     const char *port = "/dev/ttyUSB0";
@@ -47,9 +43,42 @@ int write_to_display(std::vector<uint8_t> &buf)
     write(fd, buf.data(), buf.size());
     tcdrain(fd); // wait until transmitted
 
-    uint8_t response = 0;
-    int n = read(fd, &response, 1);
-    //std::cout << response << std::endl;
+    close(fd);
+    return 0;
+}
+
+int read_fr_display(std::vector<uint8_t> &buf){
+    const char *port = "/dev/ttyUSB0";
+    int fd = open(port, O_RDWR | O_NOCTTY);
+    if (fd < 0)
+        return 1;
+
+    termios t{};
+    tcgetattr(fd, &t);
+
+    cfsetispeed(&t, B9600);
+    cfsetospeed(&t, B9600);
+
+    t.c_cflag |= (CLOCAL | CREAD);
+    t.c_cflag &= ~PARENB;
+    t.c_cflag &= ~CSTOPB;
+    t.c_cflag &= ~CSIZE;
+    t.c_cflag &= ~HUPCL;
+    t.c_cflag |= CS8;
+
+    t.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
+    t.c_iflag &= ~(IXON | IXOFF | IXANY);
+    t.c_oflag &= ~OPOST;
+
+    tcsetattr(fd, TCSANOW, &t);
+
+    int flags;
+    ioctl(fd, TIOCMGET, &flags);
+    flags |= TIOCM_DTR | TIOCM_RTS;
+    ioctl(fd, TIOCMSET, &flags);
+
+    read(fd, buf.data(), buf.size());
+    tcdrain(fd); // wait until transmitted
 
     close(fd);
     return 0;
